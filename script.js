3fd9bf199445d26ef75a15fd7496eeb6310b9e59
abhishek <57187026+anonymous-cloud@users.noreply.github.com>
2025-01-23 20:34:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const status = document.getElementById("status")
+const enterBtn = document.getElementById("enterBtn")
+//const status = document.getElementById("status")
+
+enterBtn.addEventListner("click",()=>{
+
+status.innerText = "<h1>Entered Metaverse</h1>"
+
+})
